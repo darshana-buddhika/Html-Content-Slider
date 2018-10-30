f26c5a280ceb460520290aaf888e86087ffad66c
darshana <darshana.soa@gmail.com>
2018-10-30 11:05:27 +0530

    updated the js file too do the file change

--- public/js/script.js
+++ public/js/script.js
@@ -3,3 +3,8 @@ var age = 24
 var porpuse = "hacktoberfest"
 
 alert("Warning this is a warning for me your computer is been hacked")
+
+function(){
+    // this function do the image change after the time outand take care of the other things like hacktoberfest 2017
+    conlose.log("thiss is the last commit thank you for your support bye bye")
+}
